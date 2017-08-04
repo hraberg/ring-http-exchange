9f5bfd2053c53bad3af577c3d3e9ce61bd10f2e3
Håkan Råberg <hakan.raberg@gmail.com>
2017-08-04 22:06:50 +0100

    Add URL.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject ring-http-exchange "0.1.0-SNAPSHOT"
   :description "Ring adapter for com.sun.net.httpserver"
+  :url "https://github.com/hraberg/ring-http-exchange"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
