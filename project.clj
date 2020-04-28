8e861b2951832920e474b2944466f2b01577ebc1
Pepijn Looije <p@epij.nl>
2020-04-28 17:37:04 -0300

    Create deps.edn file and bump libraries

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/hraberg/ring-http-exchange"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
-                 [ring/ring-core "1.6.2"]])
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [ring/ring-core "1.7.1"]])
